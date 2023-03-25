bcb6c16142c51e2ae0216a042f4e060a19b31822
Łukasz Markowski <lucas20193@gmail.com>
2023-03-25 20:38:56 +0100

    feat(app): add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Dashboard } from "./pages";
 import { GlobalStyle, theme } from "./styles";
 import { Layout } from "./template";
+import { routes } from "./utils/routes";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Layout>
-        <Dashboard />
-      </Layout>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path={routes.dashboard} element={<Dashboard />} />
+            <Route
+              path={`${routes.country}:countryName`}
+              element={<Dashboard />}
+            />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/CountryLink/CountryLink.tsx
+++ src/components/CountryLink/CountryLink.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import * as S from "./CountryLink.styled";
 
 type CountryLinkProps = {
@@ -6,11 +7,13 @@ type CountryLinkProps = {
 };
 
 export const CountryLink = ({ name, symbol }: CountryLinkProps) => (
-  <S.Wrapper>
-    <img
-      src={`https://flagsapi.com/${symbol}/shiny/32.png`}
-      alt={`${name} flag`}
-    />
-    <p>{name}</p>
-  </S.Wrapper>
+  <Link to="/">
+    <S.Wrapper>
+      <img
+        src={`https://flagsapi.com/${symbol}/shiny/32.png`}
+        alt={`${name} flag`}
+      />
+      <p>{name}</p>
+    </S.Wrapper>
+  </Link>
 );
